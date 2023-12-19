de4a514f13d7d34a95afe450a63643d332afadc0
HarvindG <gharvind@yahoo.co.uk>
2023-12-19 17:55:56 +0000

    working on dockerising script

--- lambda-load-old-data/lambda_load_s3.py
+++ lambda-load-old-data/lambda_load_s3.py
@@ -2,13 +2,12 @@ import csv
 
 from boto3 import client
 from botocore.exceptions import ClientError
-from os import environ, listdir
+from os import environ, listdir, path
 from dotenv import load_dotenv
 from sqlalchemy import create_engine, sql, engine
 from datetime import datetime, timedelta
 
 LOCAL_OUTPUT_DIR = "./data/"
-S3_OUTPUT_DIR = "c9-harvind-output/"
 
 
 def get_database_connection() -> engine.base.Connection:
@@ -29,8 +28,8 @@ def get_s3_client() -> client:
     return s3
 
 
-def extract_old_data_from_database(connection: engine.base.Connection):
-    """Extract data older than a day from short-term-memory as csv file"""
+def extract_old_data_from_database(connection: engine.base.Connection) -> list[tuple]:
+    """Extract data older than a day from short-term-memory as a list"""
 
     connection.execute(sql.text("USE plants;"))
 
@@ -39,16 +38,15 @@ def extract_old_data_from_database(connection: engine.base.Connection):
     # Assuming 's_delta' is the schema and 'recording' is the table
     query = sql.text("""
                      SELECT * FROM s_delta.recording
-                     WHERE CONVERT(DATE, recording.recording_taken) < :today;
+                     WHERE recording_taken < GETDATE();
                      """)
 
-    result = connection.execute(
-        query, {'today': current_date}).fetchall()
+    result = connection.execute(query).fetchall()
 
     return result
 
 
-def write_list_of_tuples_to_csv(data: list[tuple], csv_filename: str = 'latest_data.csv') -> bool:
+def write_list_of_tuples_to_csv(data: list[tuple], csv_filename: str = 'data/latest_data.csv') -> bool:
     """Write a list of tuples to a CSV file"""
 
     with open(csv_filename, 'w', newline='') as csvfile:
@@ -59,29 +57,39 @@ def write_list_of_tuples_to_csv(data: list[tuple], csv_filename: str = 'latest_d
             csv_writer.writerows(data)
 
 
+def get_master_archive_csv(s3_client: client, bucket: str, folder_name: str):
+    """Load archive csv file"""
 
-def get_master_archive_csv(s3_client:client, bucket:str, folder_name:str):
-    """Load archive csv file""" 
-    
     contents = s3_client.list_objects(Bucket=bucket)["Contents"]
     all_keys = [o["Key"] for o in contents]
-    
-    # new_k = k[17:]
 
     for k in all_keys:
         if k.endswith('.csv'):
             s3_client.download_file(bucket, k, f"{folder_name}/{k}")
 
+
+def combine_csv_files(source_csv: str, target_csv: str):
+
+    # Read data from the source CSV file
+    with open(source_csv, 'r', newline='') as source_file:
+        source_reader = csv.reader(source_file)
+        source_data = list(source_reader)
+
+    # Append data to the target CSV file
+    with open(target_csv, 'a', newline='') as target_file:
+        target_writer = csv.writer(target_file)
+        target_writer.writerows(source_data)
+
+
 def load_combined_archive_data_to_s3(s3_client, file_name, bucket, object_name=None):
     """Load old data as csv from short-term-memory into S3 then delete csv file"""
-    # If S3 object_name was not specified, use file_name
+
     if object_name is None:
         object_name = path.basename(file_name)
 
     try:
         response = s3_client.upload_file(file_name, bucket, object_name)
     except ClientError as e:
-        logging.error(e)
         return False
     return True
 
@@ -89,31 +97,28 @@ def load_combined_archive_data_to_s3(s3_client, file_name, bucket, object_name=N
 def handler(event=None, context=None):
     """Lambda handler function"""
     load_dotenv()
-    pass
+
+    conn = get_database_connection()
+    s3_client = get_s3_client()
+
+    # Retrieve yesterdays data from short-term database
+    latest_recording_data = extract_old_data_from_database(conn)
+
+    # Write yesterdays daa to csv into LOCAL_OUTPUT_DIR
+    write_list_of_tuples_to_csv(latest_recording_data, 'data/latest_data.csv')
+
+    # Retrieve master archive csv
+    get_master_archive_csv(
+        s3_client, environ['ARCHIVE_BUCKET'], LOCAL_OUTPUT_DIR)
+
+    # Append latest data csv to archive csv
+    combine_csv_files('data/latest_data.csv', 'data/archive.csv')
+
+    # Upload new archive file
+    load_combined_archive_data_to_s3(s3_client, 'data/archive.csv',
+                                     environ['ARCHIVE_BUCKET'])
 
 
 if __name__ == "__main__":
 
-    load_dotenv()
-
-    conn = get_database_connection()
-    s3_client = get_s3_client()
-    
-    # Retrieve yesterdays data from short-term database
-    list_of_recording_data = extract_old_data_from_database(conn)
-
-    # Write yesterdays daa to csv into LOCAL_OUTPUT_DIR
-    write_list_of_tuples_to_csv(list_of_recording_data, 'latest_data.csv')
-
-    # Retrieve master archive csv
-    get_master_archive_csv(s3_client, environ['ARCHIVE_BUCKET'], LOCAL_OUTPUT_DIR)
-
-    # Append latest data csv to archive csv
-
-
-
-    file_name = LOCAL_OUTPUT_DIR + ''.join([x for x in listdir(LOCAL_OUTPUT_DIR) if x.endswith('.csv')])
-    object_name = 
-
-    load_old_data_to_s3(s3_client, file_name,
-                        environ['ARCHIVE_BUCKET'], object_name)
+    handler()
