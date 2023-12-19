857f8fa7be2162ae0ac982c8a66d388800e5fe7c
Shivani Patel <shivanipatel@Shivanis-MacBook-Pro.local>
2023-12-19 16:28:50 +0000

    addition of basic functional tests

--- pipeline/seed_data.py
+++ pipeline/seed_data.py
@@ -1,19 +1,21 @@
 """Small example of connecting to an MSSQL database."""
 import csv
-from os import environ
+from os import environ, _Environ
 
 from dotenv import load_dotenv
 from sqlalchemy import create_engine, sql, Connection
 
 from transform import standardize_country_name
 
+FILEPATH = 'data/plant_data.csv'
 
-def get_connection()-> Connection:
+def get_connection(config: _Environ)-> Connection:
     """Returns a connection to the database."""
     engine = create_engine(
-        f"mssql+pymssql://{environ['DB_USERNAME']}:{environ['DB_PASSWORD']}@{environ['DB_HOST']}/?charset=utf8")
+        f"mssql+pymssql://{config['DB_USERNAME']}:{config['DB_PASSWORD']}@{config['DB_HOST']}/?charset=utf8")
     connection = engine.connect()
     return connection
+
 
 def read_csv(file_path: str) -> list[dict]:
     """Extracts the relevant information about botanists from the plant data,
@@ -60,7 +62,7 @@ def extract_plant_data(csv_data: list[dict],botanist_data: list[dict], location_
         plant_id = plant["Id"]
         name = plant['Name']
         for botanist in botanist_data:
-            if plant['Botanist Name']== botanist['name']:
+            if plant['Botanist Phone']== botanist['telephone_number']:
                 botanist_num = botanist['telephone_number']
         for location in location_data:
             if plant['Region'] == location['region']:
@@ -73,21 +75,19 @@ def extract_plant_data(csv_data: list[dict],botanist_data: list[dict], location_
 def seed_botanist_table(conn: Connection, botanist_data: list[dict]) -> None:
     """Seed the botanist table with the botanist data list."""
     for botanist in botanist_data:
-        query = sql.text("INSERT INTO s_delta.botanist (name, email, telephone_number) VALUES (:n, :e, :t)")
-        args = ({"n":botanist["name"], "e":botanist["email"], "t":botanist["telephone_number"]})
+        query = sql.text("INSERT INTO s_delta.botanist (name, email, telephone_number) VALUES (:name, :email, :num)")
+        args = ({"name":botanist["name"], "email":botanist["email"], "num":botanist["telephone_number"]})
         conn.execute(query,args)
-
 
 
 def seed_location_table(conn: Connection, location_data: list[dict]) -> None:
     """Seed the location table with the location data list."""
     for location in location_data:
         query = sql.text(
-            "INSERT INTO s_delta.location (region, country, continent) VALUES (:reg, :cou, :con)")
-        args = ({"reg": location["region"], "cou": location["country"],
-                "con": location["continent"]})
+            "INSERT INTO s_delta.location (region, country, continent) VALUES (:region, :country, :continent)")
+        args = ({"region": location["region"], "country": location["country"],
+                "continent": location["continent"]})
         conn.execute(query, args)
-
 
 
 def seed_plant_table(conn: Connection, plant_data: list[dict]) -> None:
@@ -98,13 +98,13 @@ def seed_plant_table(conn: Connection, plant_data: list[dict]) -> None:
         botanist_id = conn.execute(query,args).fetchone()[0]
 
         query = sql.text(
-            "SELECT location_id FROM s_delta.location WHERE region = (:reg)")
-        args = ({"reg": plant["location_reg"]})
+            "SELECT location_id FROM s_delta.location WHERE region = (:region)")
+        args = ({"region": plant["location_reg"]})
         location_id = conn.execute(query, args).fetchone()[0]
 
         query = sql.text(
-            "INSERT INTO s_delta.plant (plant_id, name, botanist_id, location_id) VALUES (:id, :n, :b_id, :l_id)")
-        args = ({"id": plant["plant_id"],"n": plant["name"], "b_id": botanist_id,
+            "INSERT INTO s_delta.plant (plant_id, name, botanist_id, location_id) VALUES (:id, :name, :b_id, :l_id)")
+        args = ({"id": plant["plant_id"],"name": plant["name"], "b_id": botanist_id,
                 "l_id": location_id})
         conn.execute(query, args)
 
@@ -115,14 +115,13 @@ if __name__ == "__main__":
     load_dotenv()
 
     #extracting data from .csv file
-    filepath = 'data/plant_data.csv'
-    data = read_csv(filepath)
+    data = read_csv(FILEPATH)
     botanists = extract_botanist_data(data)
     locations = extract_location_data(data)
     plants = extract_plant_data(data, botanists, locations)
 
     #connecting to database
-    db_conn = get_connection()
+    db_conn = get_connection(environ)
     db_conn.execute(sql.text("USE plants;"))
 
     #seeding each table with relevant data
