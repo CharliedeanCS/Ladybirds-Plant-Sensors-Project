4623a5e5db287c59cacb274f821e190b75cbd9aa
HarvindG <gharvind@yahoo.co.uk>
2023-12-20 12:11:08 +0000

    dockerfile working

--- lambda-load-old-data/install_sql.sh
+++ lambda-load-old-data/install_sql.sh
@@ -1,7 +1,5 @@
 
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-brew install sqlcmd
 
 brew install FreeTDS
 export CFLAGS="-I$(brew --prefix openssl)/include"export 
@@ -9,4 +7,3 @@ export LDFLAGS="-L$(brew --prefix openssl)/lib -L/usr/local/opt/openssl/lib"
 export CPPFLAGS="-I$(brew --prefix openssl)/include"
 
 pip install --pre --no-binary :all: pymssql --no-cache
-pip install sqlalchemy
